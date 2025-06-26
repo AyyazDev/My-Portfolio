7b802dc8eb5752cc708d5779d7aec45332715809
AyyazDev <ayyazfakhar@gmail.com>
2025-06-26 11:41:29 +0500

    first commit

---
+++ src/pages/Not Found.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <section className="min-h-screen flex items-center justify-center bg-gray-800 text-white">
+      <h2 className="text-4xl font-bold">404 - Page Not Found</h2>
+    </section>
+  );
+};
---
+++ src/pages/Skills.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Skills() {
+  return (
+    <section className="min-h-screen flex items-center justify-center bg-gray-600 text-white">
+      <h2 className="text-4xl font-bold">Skills Section</h2>
+    </section>
+  );
+};
